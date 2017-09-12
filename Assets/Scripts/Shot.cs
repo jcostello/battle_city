20de08279afacbae26dfa67cfda9bf819da409cc
Juan Manuel Costello <sudo.juan@gmail.com>
2017-09-12 11:30:10 -0300

    Add explosion

---
+++ Assets/Scripts/Shot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour {
+
+	public GameObject explosionPrefab;
+
+	private Rigidbody2D rigidBody;
+	private float shotLifeTime = 3f;
+	private float shotSpeed = 10f;
+
+	void Awake () {
+		rigidBody = GetComponent<Rigidbody2D>();
+	}
+
+	void Start () {
+		Destroy (gameObject, shotLifeTime);
+
+		rigidBody.velocity = transform.up * shotSpeed;
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+		Instantiate (explosionPrefab, transform.position, Quaternion.identity);
+
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/TankShooting.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankShooting : MonoBehaviour {
+
+	public GameObject shotPrefab;
+	public Transform barrelTransform;
+
+	private float fireRate = 0.5F;
+	private float nextFire = 0.0F;
+
+	void Update () {
+		float fireButton = Input.GetAxisRaw("Fire1");
+
+		if (fireButton > 0 && NextTimeToShoot()) {
+			nextFire = Time.time + fireRate;
+
+			CreateShoot();
+		}
+	}
+
+	bool NextTimeToShoot() {
+		return Time.time > nextFire;
+	}
+
+	void CreateShoot() {
+		Instantiate (shotPrefab, barrelTransform.position + barrelTransform.up * .1f, barrelTransform.rotation);
+	}
+
+}
